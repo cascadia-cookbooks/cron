2d71d6c39024de8a244670c33d7c8b5e2b5b9eb4
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-11-03 20:37:07 -0700

    Test suite

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,4 +1,22 @@
 require 'spec_helper'
 
 describe 'cas_cron::default' do
+    # Case for platform family
+    case os[:family]
+    when 'debian', 'ubuntu'
+        cron_package = 'cron'
+        cron_service = 'cron'
+    when 'redhat'
+        cron_package = 'cronie'
+        cron_service = 'crond'
+    end
+
+    describe package(cron_package) do
+        it { should be_installed }
+    end
+
+    describe service(cron_service) do
+        it { should be_enabled }
+        it { should be_running }
+    end
 end
